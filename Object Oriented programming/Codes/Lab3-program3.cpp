7fc7da03e5022f926b8ef73f93e974f28ee36706
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-08-18 12:00:04 +0530

    Create Lab3-program3.cpp

---
+++ Object Oriented programming/Codes/Lab3-program3.cpp
@@ -0,0 +1,41 @@
+//Input and display data of 5 employees using array of objects and constructors in C++
+#include <iostream>
+using namespace std;
+class Employee{
+  private:
+     string Name;
+     float salary;
+     string DOB;
+     string designation;
+  public:
+     static int i;
+     Employee(){
+         cout<<"For "<<i<<" employee"<<endl;
+         cout<<"Enter Name: ";
+         cin>>Name;
+         cout<<"Enter DOB: ";
+         cin>>DOB;
+         cout<<"Enter salary: ";
+         cin>>salary;
+         cout<<"Enter Designation: ";
+         getline(cin,designation)
+         i++;
+     }
+     void display(){
+         cout<<"Name: "<<Name<<endl;
+         cout<<"DOB: "<<DOB<<endl;
+         cout<<"Salary: "<<salary<<endl;
+         cout<<"Designation: "<<designation<<endl;
+     }
+};
+
+int Employee::i=1;
+int main(){
+    Employee arr[5];
+    for (int i=0; i<5; i++){
+        cout<<"Employee "<<i+1;
+        arr[i].display();
+        cout<<endl;
+    }
+    return 0;
+}
