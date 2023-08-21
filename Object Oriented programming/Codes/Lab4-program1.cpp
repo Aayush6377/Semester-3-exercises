ec83bf488f679dd94c2b791ae57cdbd1f88ead5c
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-08-21 13:23:41 +0530

    Update Lab4-program1.cpp

--- Object Oriented programming/Codes/Lab4-program1.cpp
+++ Object Oriented programming/Codes/Lab4-program1.cpp
@@ -1,4 +1,8 @@
-//Implement the four function calculator. Use switch case and functions
+/*Create the equivalent of a four function calculator.The program should request the user to enter a number, an operator and another
+number .It should then carry out the specified arithmetical operation: adding. subtracting, multiplying or dividing the two numbers.
+(It should use a switch statement to select the operation). Finally it should display the result. When it finishes the calculation, the
+program should ask if the user wants to do another calculation.*/
+
 #include <iostream>
 #include <math.h>
 using namespace std;
