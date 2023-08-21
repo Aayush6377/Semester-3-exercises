56e9350daf02c15648a582cd24e53bd62531116c
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-08-21 13:29:23 +0530

    Update Lab4-program2.cpp

--- Object Oriented programming/Codes/Lab4-program2.cpp
+++ Object Oriented programming/Codes/Lab4-program2.cpp
@@ -1,4 +1,7 @@
-
+/*Phone number, such as (212) 767-8900, can be thought of as having three parts: the area code (212), the exchange (767) and the number
+(8900). Write a program that uses a structure to store these three parts of a phone number separately.Call the structure phone. Create 
+two structure variables of type phone. Initialize one and have the user input a number for the other one. Then display both numbers.
+*/
 #include <iostream>
 using namespace std;
 
