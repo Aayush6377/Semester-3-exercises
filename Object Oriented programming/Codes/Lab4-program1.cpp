6b78207c5b22a0d590c55ad514c6b0c5da57b6b9
Aayush Kukreja <113628608+Aayush6377@users.noreply.github.com>
2023-08-21 09:03:02 +0530

    Create Lab4-program1.cpp

---
+++ Object Oriented programming/Codes/Lab4-program1.cpp
@@ -0,0 +1,40 @@
+//Implement the four function calculator. Use switch case and functions
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+ float a,b;
+ char c;
+ while(true){
+     cout<<"Enter expression: ";
+     cin>>a>>c>>b;
+     cout<<"Solution: ";
+     switch(c){
+      case '+':
+         cout<<a+b;
+         break;
+      case '-':
+         cout<<a-b;
+         break;
+      case '*':
+      case 'x':
+         cout<<a*b;
+         break;
+      case '/':
+         cout<<a/b;
+         break;
+      case '^':
+         cout<<pow(a,b);
+         break;
+      default:
+         cout<<"No such operator found";
+     }
+     cout<<endl;
+     bool ask;
+     cout<<"Do you want to continue (1/0): ";
+     cin>>ask;
+     if (ask==0) break;
+ }
+ return 0;
+}
